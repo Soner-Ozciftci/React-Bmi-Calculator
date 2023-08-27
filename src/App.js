e03e6df47b0ca25d10d13f12883bbc0e7e86ebbc
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-27 17:55:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+const [weight,setWeight] = useState(0);
+const [height,setHeight] = useState(0);
+const [bmi,setBmi] = useState("");
+const [image,setImage] = useState("");
+const [message,setMessage] = useState("");
+
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
